bac8e0ec3ad013d997b5924941b7c8dea2573e12
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2021-01-19 22:06:46 +0300

    second addition

--- postfix.cpp
+++ postfix.cpp
@@ -1,10 +1,9 @@
-// Copyright 2020 A.SHT
+// Copyright 2021 Schenikova
 #include "MyStack.h"
 #include "postfix.h"
 #include <iostream>
 
-int returnPriority(char a)
-{
+int returnPriority(char a) {
   if (a == '*' || a == '/')
     return 3;
   else if (a == '+' || a == '-')
@@ -17,33 +16,37 @@ std::string infix2postfix(std::string infix) {
   MyStack<char> stack(infix.size());
   std::string str_out;
   int j=0;
-  for (int i = 0; i < infix.size(); i++)
-  {
+  for (int i = 0; i < infix.size(); i++) {
     if (infix[i] == ' ')
       continue;
-    
-    if (infix[i] >= '0' && infix[i] <= '9' || infix[i] == '.')
+    if (infix[i] >= '0' && infix[i] <= '9' || infix[i] == '.') {
       str_out.push_back(infix[i]);
+      str_out.push_back(' ');
+    }
     else if (infix[i] == '(')
       stack.push(infix[i]);
-    else if (infix[i] == ')')
-    {
-      while (stack.get() != '(')
+    else if (infix[i] == ')') {
+      while (stack.get() != '(') {
         str_out.push_back(stack.pop());
+        str_out.push_back(' ');
+      }
       stack.pop();
     }
     else if (stack.isEmpty() || 
     returnPriority(stack.get()) < returnPriority(infix[i]))
       stack.push(infix[i]);
-    else if (returnPriority(stack.get()) >= returnPriority(infix[i]))
-    {
-      while (!stack.isEmpty() && returnPriority(stack.get()) >= returnPriority(infix[i]))
+    else if (returnPriority(stack.get()) >= returnPriority(infix[i])) {
+      while (!stack.isEmpty() && returnPriority(stack.get()) >= returnPriority(infix[i])) {
         str_out.push_back(stack.pop());
+        str_out.push_back(' ');
+      }
       stack.push(infix[i]);
     }
   } 
-  while (!stack.isEmpty())
+  while (!stack.isEmpty()) {
     str_out.push_back(stack.pop());
-  
+    str_out.push_back(' ');
+  }
+  str_out.pop_back();
   return str_out;
 }
